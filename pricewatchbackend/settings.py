b9f63e3a0950d2b2e242193ad7d43b8397e7111c
shaphat-pixel <khalshaphat@gmail.com>
2023-03-04 14:19:22 +0000

    initial commit

--- pricewatchbackend/settings.py
+++ pricewatchbackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'pricewatchbackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'build/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
--- pricewatchbackend/urls.py
+++ pricewatchbackend/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 from deals.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', TemplateView.as_view(template_name='index.html')),
 
     path('categories/', CategoryView, name="categories-list"),
     path('categories-create/', CategoryCreate, name="categories-create"),
